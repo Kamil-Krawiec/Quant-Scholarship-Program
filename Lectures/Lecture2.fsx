926968944dcbedcdb70c0ec81d73a0d87de56bea
Kamil Krawiec <kamil.krawiec9977@gmail.com>
2022-01-03 19:51:56 +0100

    new look

--- Lectures/Lecture2.fsx
+++ Lectures/Lecture2.fsx
@@ -19,32 +19,21 @@ ListFoldBack (+) [1..10] 0 = List.foldBack (+) [1..10] 0;;
 
 // Ex 2
 
-let SumAhead list=
-    let firstSum= list |> ListFold (fun h acc -> if(h<0) then acc else acc+h) 0
-    let wholeSum = list |> ListFold (+) 0
-    wholeSum,firstSum
-// List.fold_left (fun (s, p) h -> (s + h, p * h)) (0, 1) xs;;
-SumAhead [3;3;-1;5;5;-2;6;4;-3;8;2]
-SumAhead []
-SumAhead [1;2;3]
-
 //Ex 4
 let countNonWhiteChar list=
+    list 
+    |> List.filter (fun x-> not(System.String.IsNullOrWhiteSpace(x))) 
+    |> List.map (fun x-> x.Trim()) 
+    |> List.mapi (fun i x -> i+ String.length x )
+    |> List.fold (+) 0
 
-    let newList = 
-        list 
-        |> List.filter (fun x-> not(System.String.IsNullOrWhiteSpace(x))) 
-        |> List.map (fun x-> x.Trim()) 
-        |> List.mapi (fun i x -> i+ String.length x )
-        |> List.fold (+) 0
-        
-    newList
-
-    
 countNonWhiteChar [" ala";"";"ma ";" ";"kota";" "]=12;;
 countNonWhiteChar [" "]=0;;
 countNonWhiteChar []=0;;
 countNonWhiteChar ["";" "]=0;;
 
+// Ex 5
+
+let permutation list=
 
 
