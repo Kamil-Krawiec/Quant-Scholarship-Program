3632536cc365196f4e105766e593ce150922f96b
Kamil Krawiec <kamil.krawiec9977@gmail.com>
2022-01-03 20:17:26 +0100

    Ex2 done

--- Lectures/Lecture2.fsx
+++ Lectures/Lecture2.fsx
@@ -19,6 +19,29 @@ ListFoldBack (+) [1..10] 0 = List.foldBack (+) [1..10] 0;;
 
 // Ex 2
 
+let rec sumAhead list=
+    match list with
+    h::_ when h<0 -> list |> List.fold (+) 0
+    |_::t->  sumAhead t
+    |[] -> 0
+
+let rec sumAheadMyImp list=
+    match list with
+    h::_ when h<0 -> list |> ListFold (+) 0
+    |_::t->  sumAhead t
+    |[] -> 0
+
+
+// TEST
+sumAheadMyImp [3;3;-1;5;5;-2;6;4;-3;8;2] =24;;
+sumAheadMyImp [1;2;3] = 0;;
+sumAhead [3;3;-1;5;5;-2;6;4;-3;8;2] =24;;
+sumAhead [1;2;3] = 0;;
+
+// Ex3
+let rec sumBack list=
+    
+
 //Ex 4
 let countNonWhiteChar list=
     list 
